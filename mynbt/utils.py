cf593661a3dc4ef723fcbb118e9c7543465f8d87
Sonia <sonia@salameche.kanto.pkmn>
2019-10-05 14:38:11 +0200

    Recursive implementation of the slice util

---
+++ mynbt/utils.py
@@ -0,0 +1,54 @@
+def rslice(base, start, stop):
+    return Slice(base, start, stop)
+
+class Slice:
+    def __init__(self, base, start, stop):
+        if start is None:
+            start = 0
+        elif start < 0:
+            start += len(base)
+        if stop is None:
+            stop = len(base)
+        elif stop < 0:
+            stop += len(base)
+
+        if stop > len(base):
+            stop = len(base)
+
+        if start > stop:
+            start = stop
+
+        self.base = base
+        self.start = start
+        self.stop = stop
+
+    def __repr__(self):
+        return "Slice({base},{start},{stop})".format(**self.__dict__)
+
+    def __len__(self):
+        return self.stop-self.start
+
+    def __eq__(self,other):
+        if type(other) is Slice:
+            other = [*other]
+
+        if type(other) is list:
+            return [*self] == other
+
+        return super().__eq__(other)
+
+    def __getitem__(self,idx):
+        if type(idx) is int:
+            idx += self.start if idx >= 0 else self.stop
+            if idx >= self.stop or idx < self.start:
+                raise IndexError
+            
+            return self.base[idx]
+        elif type(idx) is slice:
+            if idx.step is not None:
+                raise TypeError("step slices are not supported")
+            return Slice(self.base,
+                    idx.start + (self.start if idx.start >= 0 else self.stop),
+                    idx.stop + (self.start if idx.stop >= 0 else self.stop))
+                        
+
---
+++ test/test_utils.py
@@ -0,0 +1,51 @@
+import unittest
+
+from mynbt.utils import rslice
+
+class TestRefSlices(unittest.TestCase):
+    def test_slice_item_access(self):
+        a = [*range(10)]
+        b = rslice(a,1,4)
+        self.assertEqual(b[0], 1)
+        self.assertEqual(b[1], 2)
+        self.assertEqual(b[2], 3)
+
+    def test_slice_item_access_neg(self):
+        a = [*range(10)]
+        b = rslice(a,1,4)
+        self.assertEqual(b[-1], 3)
+        self.assertEqual(b[-2], 2)
+        self.assertEqual(b[-3], 1)
+
+    def test_slice_item_out_of_bounds(self):
+        a = [*range(10)]
+        b = rslice(a,1,4)
+
+        for idx in -100,-4,3,100:
+            with self.assertRaises(IndexError, msg=idx):
+                b[idx]
+
+    def test_slice_array(self):
+        a = [*range(10)]
+        b = rslice(a,1,4)
+        self.assertEqual(b, a[1:4])
+
+    def test_slice_array_reversed(self):
+        a = [*range(10)]
+        b = rslice(a,4,1)
+        self.assertEqual(b, a[4:1])
+
+    def test_slice_from_end(self):
+        a = [*range(10)]
+        b = rslice(a,-1,-4)
+        self.assertEqual(b, a[-1:-4])
+
+    def test_slice_from_end_reversed(self):
+        a = [*range(10)]
+        b = rslice(a,-4,-1)
+        self.assertEqual(b, a[-4:-1])
+
+    def test_compatibility_with_byte_string(self):
+        a = b"012345678"
+        b = rslice(a,1,4)
+        self.assertEqual(bytes(b), b"123")
