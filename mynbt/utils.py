77b63a2e4652d6e618470138274171f6c8ac68f8
Sylvain Leroux <sylvain@chicoree.fr>
2019-10-27 01:38:02 +0200

    Make compact hexdumps

--- mynbt/utils.py
+++ mynbt/utils.py
@@ -1,7 +1,7 @@
 hexdump_map_hex = tuple( format(i, '02x') for i in range(256) )
 hexdump_map_txt = "".join(("."*32, *(chr(i) for i in range(32,127)), "."*129))
 
-def hexdump(data):
+def hexdump(data, maxlines=-1, compact=True):
     """ Produce an hex/ascii dump of the data
     """
     def _dumpline(line):
@@ -9,8 +9,12 @@ def hexdump(data):
 
     data = memoryview(data)
     addr = 0
-    while True:
+    previous = ""
+    starred = False
+    while maxlines != 0:
         if not data:
+            if starred:
+                yield previous
             break
 
         line, data = data[:16], data[16:]
@@ -18,6 +22,22 @@ def hexdump(data):
         hex = " ".join(hexdump_map_hex[b] for b in line)
         ascii = "".join(hexdump_map_txt[b] for b in line)
 
-        yield "{:06x}    {:50s} |{:8s}|".format(addr, hex, ascii)
+        line = "{:06x}    {:50s} |{:8s}|".format(addr, hex, ascii)
+        if not compact or maxlines==1:
+            yield line
+        else:
+            if line[6:] == previous[6:]:
+                if not starred:
+                    starred = True
+                    yield '*'
+            else:
+                if starred and previous:
+                    yield previous
+
+                starred = False
+                yield line
+
+        previous = line
+        maxlines -= 1
         addr += 16
 
