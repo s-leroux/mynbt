917998219bf9736a71da086654a4449b07ce3091
Sylvain Leroux <sylvain@chicoree.fr>
2019-11-01 01:31:22 +0100

    Update entity pos while moving chunks

--- mynbt/region.py
+++ mynbt/region.py
@@ -21,5 +21,15 @@ class Region(Anvil):
         nbt.xPos = 32*self._rx+ci.x
         nbt.zPos = 32*self._rz+ci.z
 
+        try:
+            for entity in nbt.Level.Entities:
+                ex = entity.Pos[0] % 32
+                ez = entity.Pos[2] % 32
+
+                entity.Pos[0] = ex + 32*ci.x + 32*32*self._rx
+                entity.Pos[2] = ez + 32*ci.z + 32*32*self._rz
+        except KeyError:
+            pass
+
         return self.write_chunk(ci.x, ci.z, nbt, timestamp=ci.timestamp)
 
--- test/test_region.py
+++ test/test_region.py
@@ -7,19 +7,35 @@ from test.data.nbt import *
 class TestRegion(unittest.TestCase):
     RX = 10
     RZ = 20
+    C1X = 1
+    C1Z = 2
+    C2X = 3
+    C2Z = 4
+
     R = REGION(10*PAGE_SIZE,
-      CHUNK(1,2,pageaddr=3,data=CHUNK_DATA(
+      CHUNK(C1X,C1Z,pageaddr=3,data=CHUNK_DATA(
           COMPOUND_FRAME(
               STRING_FRAME("Chunk 1,2", "n"),
-              INT_FRAME(32*RX+1, "xPos"),
-              INT_FRAME(32*RZ+2, "zPos"),
+              INT_FRAME(32*RX+C1X, "xPos"),
+              INT_FRAME(32*RZ+C1Z, "zPos"),
+              WITH_NAME("Level",COMPOUND_FRAME)(
+                  WITH_NAME("Entities",LIST_FRAME)(
+                      COMPOUND,
+                      [
+                          WITH_NAME("Pos",LIST_FRAME)(
+                              SHORT,
+                              [32*32*RX+32*C1X + 11, 0, 32*32*RZ+32*C1Z + 22]
+                          ),
+                      ]
+                  ),
+              ),
           ),
       )),
-      CHUNK(3,4,pageaddr=4,data=CHUNK_DATA(
+      CHUNK(C2X,C2Z,pageaddr=4,data=CHUNK_DATA(
           COMPOUND_FRAME(
               STRING_FRAME("Chunk 3,4", "n"),
-              INT_FRAME(32*RX+3, "xPos"),
-              INT_FRAME(32*RZ+4, "zPos"),
+              INT_FRAME(32*RX+C2X, "xPos"),
+              INT_FRAME(32*RZ+C2Z, "zPos"),
           ),
       )),
     )
@@ -28,12 +44,22 @@ class TestRegion(unittest.TestCase):
         self.region = Region(self.RX, self.RZ, self.R)
 
     def test_1(self):
-        """ Region can copy chunks
+        """ Region can copy chunks and update xPos and zPos
         """
-        self.region.chunk[3,4] = self.region.chunk[1,2]
+        self.region.chunk[self.C2X,self.C2Z] = self.region.chunk[self.C1X,self.C1Z]
 
-        nbt = self.region.chunk[3,4].parse()
+        nbt = self.region.chunk[self.C2X,self.C2Z].parse()
         self.assertEqual(nbt.n, "Chunk 1,2")
-        self.assertEqual(nbt.xPos, 320+3)
-        self.assertEqual(nbt.zPos, 640+4)
+        self.assertEqual(nbt.xPos, 32*self.RX+self.C2X)
+        self.assertEqual(nbt.zPos, 32*self.RZ+self.C2Z)
+
+
+    def test_2(self):
+        """ Region can copy chunks and update entities pos
+        """
+        self.region.chunk[self.C2X,self.C2Z] = self.region.chunk[self.C1X,self.C1Z]
+
+        nbt = self.region.chunk[self.C2X,self.C2Z].parse()
+        self.assertEqual(nbt.Level.Entities[0].Pos[0], 32*32*self.RX + 32*self.C2X + 11)
+        self.assertEqual(nbt.Level.Entities[0].Pos[2], 32*32*self.RZ + 32*self.C2Z + 22)
 
